16276a03ff61078ae12bad469bec300316cab9e6
Shital Talekar <Shital Talekar@LAPTOP-T9H05E2M>
2022-08-09 12:57:47 +0530

    First Commit

---
+++ FirstProject/src/ForLoop.java
@@ -0,0 +1,27 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int length = 3;
+		int key = 1;
+		int k = 0;
+		out:{
+			for(int i=0; i<length; i++)
+			{
+				in:{
+					for(int j=0; j<length; j++)
+					{
+						if(i==key)
+						{
+							break in;
+						}
+						k=k+1;
+					}
+				}
+			}
+		}
+		 System.out.println(k);
+
+	}
+
+}
