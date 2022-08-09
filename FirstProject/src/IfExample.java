768cb04260cedbfd152ac9fb23ccc678c6aced03
Shital Talekar <Shital Talekar@LAPTOP-T9H05E2M>
2022-08-09 13:01:38 +0530

    First Commit

---
+++ FirstProject/src/IfExample.java
@@ -0,0 +1,26 @@
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		  int a=1;
+		   if(a==1)
+		   {
+			   System.out.println("One");
+		   }
+		   else if(a==2)
+		   {
+			   System.out.println("Two");  
+		   }
+		   else if(a==3)
+		   {
+			   System.out.println("Three");
+		   }
+		   else
+		   {
+			   System.out.println("Exit");
+		   }
+			     
+
+	}
+
+}
