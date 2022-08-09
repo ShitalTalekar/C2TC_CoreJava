127b2b894e74d26acf0b775161923c84f7987376
Shital Talekar <Shital Talekar@LAPTOP-T9H05E2M>
2022-08-09 12:52:22 +0530

    First Commit

---
+++ FirstProject/src/TernaryOperator.java
@@ -0,0 +1,15 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int v = 1;
+		System.out.println(v == 1 ? "A" : "B");
+		
+		v++;
+		
+		System.out.println(v == 1 ? "A" : "B");
+		 
+
+	}
+
+}
