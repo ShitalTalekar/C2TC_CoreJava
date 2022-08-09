fd8632bc7961653ecad7f1fdabc617e6ffb98812
Shital Talekar <Shital Talekar@LAPTOP-T9H05E2M>
2022-08-09 12:41:15 +0530

    First Commit

---
+++ FirstProject/src/LeftShift.java
@@ -0,0 +1,15 @@
+
+public class LeftShift {
+
+	public static void main(String[] args) {
+		int i;
+		 int num = 16;
+		 for(i=0; i<4; i++)
+		 {
+			 num = num >> 1;
+		 System.out.println(num);
+		 }
+
+	}
+
+}
